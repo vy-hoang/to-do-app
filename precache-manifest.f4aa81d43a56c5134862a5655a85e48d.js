6eb02feb64a338503fdedc41a6ad63bfb5306ac8
vy-hoang <57076819+vy-hoang@users.noreply.github.com>
2020-07-21 17:06:33 -0400

    Updates

---
+++ precache-manifest.f4aa81d43a56c5134862a5655a85e48d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0a05c81dffa509be3a03f2588d9592",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "11b4b3c232c8c8b4b4c7",
+    "url": "/to-do-app/static/js/2.a7255aaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.a7255aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff0082b9c83c7023980",
+    "url": "/to-do-app/static/js/main.2d71f475.chunk.js"
+  },
+  {
+    "revision": "c95d1af64bffd25ca0b2",
+    "url": "/to-do-app/static/js/runtime-main.127d3f8f.js"
+  }
+]);
